8dcb2dcf78ea0319eabeb7e4c3be3e7793f8f68f
Donnis Moore <70178424+powerreddude@users.noreply.github.com>
2021-09-18 09:50:04 -0500

    resaved on vscode and upladed to fix faulty upload
    resaved on vscode and upladed to fix faulty upload

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -11,7 +11,27 @@ Call multiply_numbers with 10 and 10 parameter values and display function resul
 */
 int main()
 {
-	
+	double meal_amount;
+
+	double tip_rate;
+
+	double tip_amount;
+
+	double tax_amount;
+
+	double total;
+
+	cin>>meal_amount;
+
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cin>>tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = meal_amount + tip_amount + tax_amount;
+
+	cout<<"\nMeal Amount:\t"<<meal_amount<<"\nSales Tax:\t"<<tax_amount<<"\nTip Amount:\t"<<tip_amount<<"\nTotal:\t"<<total;
 
 	return 0;
 }
