2f0eb83d41b676123a325f91785e1dce67891d7e
Donnis Moore <70178424+powerreddude@users.noreply.github.com>
2021-09-17 23:53:55 -0500

    Homework 2 commit
    Homework 2 commit

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,8 +1,10 @@
 ﻿//write include statements
-
+#include <iostream>
+#include "hwexpressions.h"
 
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
