515edc58e4de9f05617923309af5e8c2e04f5d87
Donnis Moore <70178424+powerreddude@users.noreply.github.com>
2021-09-24 23:47:47 -0500

    hw 3 commit
    hw 3 commit

--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,8 +1,14 @@
 //write include required statements
-
+#include <string>
 
 //Write a function(s) prototype  here
 
+std::string get_letter_grade_using_if(int grade);
 
+std::string get_letter_grade_using_switch(int grade);
 
+void print_main_menu();
 
+int get_int(std::string);
+
+void run_grade_to_letter_grade_conversion();
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,13 @@
 //write include statements
+#include "decisions.h"
+#include <string>
+#include <iostream>
 
 int main() 
 {
+	print_main_menu();
+
+	run_grade_to_letter_grade_conversion();
+	
 	return 0;
 }
