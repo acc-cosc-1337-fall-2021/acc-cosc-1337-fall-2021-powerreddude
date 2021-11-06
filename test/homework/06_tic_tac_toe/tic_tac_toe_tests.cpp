4be96a1a6b46a1b184829551ac77b2b6e420976a
6-toppings-1099 <86387658+6-toppings-1099@users.noreply.github.com>
2021-11-05 23:21:37 -0500

    hw 8
    hw 8

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "tic_tac_toe.h"
+#include "tic_tac_toe_manager.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -188,4 +189,43 @@ TEST_CASE("Test win diagonally from bottom left", "verification") {
 	
 	REQUIRE(game.game_over());
 	REQUIRE(game.get_winner() == "X");
+}
+
+TEST_CASE("Test TicTacToe manager get winner total function - simulate playing 3 or more games with X, O, and C as winners-verify that that x_win, o_win, and ties tally correctly", "verification") {
+	Tic_tac_toe game;
+	Tic_tac_toe_manager manager;
+
+	game.start_game("X");
+
+	game.mark_board(7);
+	game.mark_board(4);
+	game.mark_board(5);
+	game.mark_board(6);
+	game.mark_board(3);
+
+	manager.save_game(game);
+
+	game.start_game("O");
+
+	game.mark_board(7);
+	game.mark_board(4);
+	game.mark_board(5);
+	game.mark_board(6);
+	game.mark_board(3);
+
+	manager.save_game(game);
+
+	game.start_game("X");
+
+	game.mark_board(1);
+	game.mark_board(3);
+	game.mark_board(2);
+	game.mark_board(4);
+	game.mark_board(6);
+	game.mark_board(5);
+	game.mark_board(7);
+	game.mark_board(8);
+	game.mark_board(9);
+
+	manager.save_game(game);
 }
